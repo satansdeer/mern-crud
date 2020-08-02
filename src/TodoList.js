5b552ddcf2b74beb388acc4781631d525c7cf3f9
Maksim Ivanov <satansdeer@gmail.com>
2020-08-02 18:30:47 +0200

    Add table

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Link } from 'react-router-dom'
 
 export const TodoList = () => {
-  return <div className="container">TodoList</div>
-}
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    setItems([
+      {text: "foo", id: 0},
+      {text: "bar", id: 1},
+      {text: "bazz", id: 2},
+    ])
+  }, [])
+
+  return (
+    <div className="container">
+      <div className="mt-3">
+        <h3>Todo List</h3>
+        <table className="table table-striped mt-3">
+          <thead>
+            <tr>
+              <th>Text</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              items.map(todo => (
+                <tr key={todo.id}>
+                  <td>
+                    {todo.text}
+                  </td>
+                  <td>
+                    <Link to={`/edit/${todo.id}`}>Edit</Link>
+                  </td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
